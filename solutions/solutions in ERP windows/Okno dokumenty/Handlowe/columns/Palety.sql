54cca30f004a99482db50fba0cfb3afc48a53ca6
Arkadiusz <arkadiusz@ceim.pl>
2026-03-16 12:02:00 +0100

    fix: Palety Handlowe — {filtrsql} przeniesione na zewnetrzny WHERE

--- solutions/solutions in ERP windows/Okno dokumenty/Handlowe/columns/Palety.sql
+++ solutions/solutions in ERP windows/Okno dokumenty/Handlowe/columns/Palety.sql
@@ -1,14 +1,14 @@
-select
-isnull(stuff((select ', ' + [Rodzaj palety] + ': ' + [Ilość]
-from
-(select lt.Name as [Rodzaj palety], convert(varchar(5),count(distinct lo.barcode)) as [Ilość]
-from CDN.TraNag
-join CDN.MagNag on MaN_ZrdTyp = TrN_GIDTyp and MaN_ZrdNumer = TrN_GIDNumer
-join wms.documents d with (nolock) on d.SourceDocumentId = MaN_GIDNumer
-join wms.items i with (nolock) on d.id=i.DocumentId
-join wms.LogisticUnitObjects lo with (nolock) on i.LogisticUnitId = lo.Id
-join wms.LogisticUnitTypes lt with (nolock) on lo.LogisticsUnitTypeId=lt.Id
-where {filtrsql}
-group by lt.name
-having count(distinct lo.barcode) <> 0) cte
-for xml path ('')),1,2,''),'') as [Palety]
+SELECT
+    isnull(stuff((
+        select ', ' + lt.Name + ': ' + convert(varchar(5), count(distinct lo.barcode))
+        from CDN.MagNag m
+        join wms.documents d with (nolock) on d.SourceDocumentId = m.MaN_GIDNumer
+        join wms.items i with (nolock) on d.id=i.DocumentId
+        join wms.LogisticUnitObjects lo with (nolock) on i.LogisticUnitId = lo.Id
+        join wms.LogisticUnitTypes lt with (nolock) on lo.LogisticsUnitTypeId=lt.Id
+        where m.MaN_ZrdNumer = TrN_GIDNumer and m.MaN_ZrdTyp = TrN_GIDTyp
+        group by lt.Name
+        having count(distinct lo.barcode) <> 0
+        for xml path('')), 1, 2, ''), '') as [Palety]
+FROM CDN.TraNag
+WHERE {filtrsql}
