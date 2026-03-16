8b34f1085edc5d75bf314701e6d8c2ca012e0e67
Arkadiusz <arkadiusz@ceim.pl>
2026-03-16 10:41:41 +0100

    feat: kolumna Termin platnosci w Okno dokumenty/Handlowe

---
+++ solutions/solutions in ERP windows/Okno dokumenty/Handlowe/columns/Termin płatności.sql
@@ -0,0 +1,7 @@
+SELECT
+    CASE WHEN MAX(TrP_Termin) = 0 THEN NULL
+         ELSE CAST(DATEADD(d, MAX(TrP_Termin), '18001228') AS DATE)
+    END [Termin płatności]
+FROM CDN.TraPlat
+JOIN CDN.TraNag ON TrN_GIDNumer = TrP_GIDNumer AND TrN_GIDTyp = TrP_GIDTyp
+WHERE {filtrsql}
