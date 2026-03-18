22d37347dad4b70580eb135eb40e6061e1a25595
Arkadiusz <arkadiusz@ceim.pl>
2026-03-18 12:08:49 +0100

    fix: formatowanie SQL — Przeterminowane naleznosci (PLN)

--- solutions/solutions in ERP windows/Okno lista zamówień sprzedaży/Zamówienia/columns/Przeterminowane należności (PLN).sql
+++ solutions/solutions in ERP windows/Okno lista zamówień sprzedaży/Zamówienia/columns/Przeterminowane należności (PLN).sql
@@ -1,1 +1,11 @@
-SELECT\n    SUM(TrP_PozostajeSys) [Przeterminowane należności (PLN)]\nFROM cdn.ZamNag\nJOIN cdn.TraPlat\n    ON TrP_KntNumer = ISNULL(NULLIF(ZaN_KnPNumer, 0), ZaN_KntNumer)\n    AND TrP_KntTyp  = ISNULL(NULLIF(ZaN_KnPTyp,  0), ZaN_KntTyp)\nWHERE TrP_Typ = 2\n    AND TrP_Rozliczona = 0\n    AND TrP_Pozostaje > 0\n    AND TrP_Termin < DATEDIFF(day, '18001228', GETDATE())\n    AND {filtrSQL}
+SELECT
+    SUM(TrP_PozostajeSys) [Przeterminowane należności (PLN)]
+FROM cdn.ZamNag
+JOIN cdn.TraPlat
+    ON TrP_KntNumer = ISNULL(NULLIF(ZaN_KnPNumer, 0), ZaN_KntNumer)
+    AND TrP_KntTyp  = ISNULL(NULLIF(ZaN_KnPTyp,  0), ZaN_KntTyp)
+WHERE TrP_Typ = 2
+    AND TrP_Rozliczona = 0
+    AND TrP_Pozostaje > 0
+    AND TrP_Termin < DATEDIFF(day, '18001228', GETDATE())
+    AND {filtrSQL}
